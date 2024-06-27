64fad8cce83bcb823b4661e4f3491dac6a680ea6
Oleksandr Merkulov <almerico@gmail.com>
2024-06-27 11:21:13 -0400

    all to 443

--- front-end/cmd/web/main.go
+++ front-end/cmd/web/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/validators", func(w http.ResponseWriter, r *http.Request) {
 		render(w, "test.page.gohtml")
 	})
 
